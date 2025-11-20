bdb457d69ef6b25f3d695e0f9be5d751372f135b
brixhab <brixhab@gmail.com>
2025-11-20 14:31:52 +0530

    Update google-button.php

--- templates/google-button.php
+++ templates/google-button.php
@@ -1,6 +1,7 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
-
-<a href="<?php echo esc_url( $google_url ); ?>" class="elhp-google-btn">
-    <img src="https://developers.google.com/identity/images/g-logo.png" class="elhp-g-icon">
-    Continue with Google
-</a>
+<div class="elhp-google-wrap">
+    <a href="<?php echo esc_url( $google_url ); ?>" class="elhp-google-btn" rel="nofollow">
+        <img src="https://developers.google.com/identity/images/g-logo.png" class="elhp-g-icon" alt="G">
+        <span>Continue with Google</span>
+    </a>
+</div>
