ed1ae9884d24cc159e83b97a80750f05481c1079
brixhab <brixhab@gmail.com>
2025-11-20 14:22:42 +0530

    Create google-button.php

---
+++ templates/google-button.php
@@ -0,0 +1,6 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
+
+<a href="<?php echo esc_url( $google_url ); ?>" class="elhp-google-btn">
+    <img src="https://developers.google.com/identity/images/g-logo.png" class="elhp-g-icon">
+    Continue with Google
+</a>
