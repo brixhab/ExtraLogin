e8437a8fd4184380d73eb80b6437046e7836d819
brixhab <brixhab@gmail.com>
2025-11-20 14:30:56 +0530

    Update class-google-button.php

--- includes/class-google-button.php
+++ includes/class-google-button.php
@@ -1,26 +1,29 @@
 <?php
-
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class ELHP_Google_Button {
-
     public function __construct() {
         add_filter( 'hivepress/v1/templates/user_login_block', [ $this, 'add_google_button' ], 20 );
         add_filter( 'hivepress/v1/templates/user_register_block', [ $this, 'add_google_button' ], 20 );
+        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets' ] );
+    }
+
+    public function enqueue_assets() {
+        wp_register_style( 'elhp-style', ELHP_URL . 'assets/google-button.css' );
+        wp_enqueue_style( 'elhp-style' );
     }
 
     public function add_google_button( $template ) {
-
         $google_auth_url = site_url( '/?elhp_google_login=1' );
 
-        $template['blocks']['google_login'] = [
+        // Add to template blocks
+        $template['blocks']['elhp_google_login'] = [
             'type'    => 'part',
             'path'    => ELHP_PATH . 'templates/google-button.php',
             'context' => [
                 'google_url' => $google_auth_url
             ]
         ];
-
         return $template;
     }
 }
