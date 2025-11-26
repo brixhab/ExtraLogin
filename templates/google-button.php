e6bf9eb6f8e65d31d937cc9354984f4270e8ba52
brixhab <brixhab@gmail.com>
2025-11-26 14:32:02 +0530

    Delete templates directory

--- templates/google-button.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
-<div class="elhp-google-wrap">
-    <a href="<?php echo esc_url( $google_url ); ?>" class="elhp-google-btn" rel="nofollow">
-        <img src="https://developers.google.com/identity/images/g-logo.png" class="elhp-g-icon" alt="G">
-        <span>Continue with Google</span>
-    </a>
-</div>
